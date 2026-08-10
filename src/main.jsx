411a5653cd3d3e2af39a18d640cb6299932f4a13
Hadia <hadiashah010@gmail.com>
2026-08-10 16:39:31 +0500

    add transaction management functions to createBankAccount

--- src/main.jsx
+++ src/main.jsx
@@ -11,10 +11,31 @@ function createBankAccount(userFullName, branchInfo, depositAmount) {
   const monthlyCreditLimit = 150000;
   const monthlyDebitLimit = 150000;
   let userBalance = 0;
+  let creditedToday= 0;
+  let debitedToday = 0;
+  let TransactionRecords=[]
+  function updateCredits(credits) {
+    if (credits + creditedToday <= dailyCreditLimit) {
+      userBalance += credits;
+      creditedToday += credits;
+       TransactionRecords.push({userBalance,actionType:credit,time: new Date()})
+    }
+  }
+  function updateDebits(debits, targetUseraccount) {
+    if (debits + debitedToday <= dailydebitLimit) {
+      userBalance -= debits;
+      targetUseraccount += debits;
+      debitedToday += debits;
+      TransactionRecords.push({userBalance,actionType:debit,time: new Date()})
+    }
+  }
+  function TransactionHistory() {
+    return TransactionRecords;
+  }
   return (accountDetails = {
     userFullName,
     accountNumber: 675445266598,
-    IbanNumber: branchCode + "675445",
+    IbanNumber: branchCode + "675445"
   });
 }
 
