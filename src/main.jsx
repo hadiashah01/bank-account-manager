e75b95c268b37b4e6d9f5f48a36f94223defadaa
Hadia <hadiashah010@gmail.com>
2026-08-10 15:13:51 +0500

    feat: implement createBankAccount function for account management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+function createBankAccount(userFullName, branchInfo, depositAmount) {
+  let branchName = branchInfo.branchName;
+  let branchCode = branchInfo.branchCode;
+  const dailyCreditLimit = 5000;
+  const dailyDebitLimit = 5000;
+  const monthlyCreditLimit = 150000;
+  const monthlyDebitLimit = 150000;
+  let userBalance = 0;
+  return (accountDetails = {
+    userFullName,
+    accountNumber: 675445266598,
+    IbanNumber: branchCode + "675445",
+  });
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
