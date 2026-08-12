394de06c8d2ccf49fdd466e4fa7ab38b0689b7db
Hadia <hadiashah010@gmail.com>
2026-08-12 17:09:39 +0500

    integrate bank account methods with React state in Appjsx
    move thr create createBankAccount from main.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,42 +6,111 @@ import AvailableBalance from "./components/AvailableBalance/AvailableBalance";
 import { useState } from "react";
 
 export default function App() {
-  const [accounts, setAccounts] = useState([
-    { id: 1234, type: "CHECKING", balance: 12450, holder: "John Doe" },
-    {
-      id: 5678,
-      type: "SAVINGS",
-      balance: 40120,
-      holder: "John Doe",
-    },
-    {
-      id: 9112,
-      type: "CREDITS",
-      balance: -12450,
-      holder: "John Doe",
-    },
-  ]);
-  const [activeAccountId, setActiveAccountId] = useState(1234);
+  function createBankAccount(userFullName, branchInfo, depositAmount) {
+    const accountNumber =
+      Math.floor(Math.random() * 900000000000) + 100000000000;
+    let branchName = branchInfo.branchName;
+    let branchCode = branchInfo.branchCode;
+    const dailyCreditLimit = 5000;
+    const dailyDebitLimit = 5000;
+    const monthlyCreditLimit = 150000;
+    const monthlyDebitLimit = 150000;
+    let userBalance = depositAmount;
+    let creditedToday = 0;
+    let debitedToday = 0;
+    let TransactionRecords = [];
+    function updateCredits(credits) {
+      if (credits + creditedToday <= dailyCreditLimit) {
+        userBalance += credits;
+        creditedToday += credits;
 
-  const onDebit = (amount, targetId) => {
-    if (!amount || !targetId) return;
-    setAccounts((prev) =>
-      prev.map((acc) => {
-        return acc.id == targetId
-          ? { ...acc, balance: acc.balance - Number(amount) }
-          : acc;
-      }),
-    );
+        TransactionRecords.push({
+          userBalance,
+          actionType: " credit",
+          time: new Date(),
+        });
+        return userBalance;
+      }
+    }
+    function updateDebits(debits, targetUseraccount) {
+      if (debits + debitedToday <= dailyDebitLimit) {
+        userBalance -= debits;
+        targetUseraccount.balance += debits;
+        debitedToday += debits;
+        TransactionRecords.push({
+          userBalance,
+          actionType: "debit",
+          time: new Date(),
+        });
+        return userBalance;
+      }
+    }
+    function TransactionHistory() {
+      return TransactionRecords;
+    }
+    return {
+      userFullName,
+      accountNumber,
+      IbanNumber: branchCode + accountNumber,
+      get balance() {
+        return userBalance;
+      },
+      updateDebits,
+      updateCredits,
+      TransactionHistory,
+    };
+  }
+  const mainBranch = {
+    branchName: "Main Branch",
+    branchCode: 567,
+  };
+
+  const cityBranch = {
+    branchName: "City Branch",
+    branchCode: 789,
+  };
+
+  const canttBranch = {
+    branchName: "Cantt Branch",
+    branchCode: 345,
+  };
+  const initialAccounts = [
+    createBankAccount("Ali", mainBranch, 12000),
+    createBankAccount("Ahmed", cityBranch, 20000),
+    createBankAccount("Sara", canttBranch, 15000),
+  ];
+
+  const [accounts, setAccounts] = useState(initialAccounts);
+  const [activeAccountNumber, setActiveAccountNumber] = useState(
+    accounts[0].accountNumber,
+  );
+
+  const onDebit = (amount, targetAccountNumber) => {
+    if (!amount || !targetAccountNumber) return;
+    setAccounts((prev) => {
+      const UpdatedAccounts = [...prev];
+      const targetAccount = UpdatedAccounts.find(
+        (acc) => acc.accountNumber == targetAccountNumber,
+      );
+      const senderAccount = UpdatedAccounts.find(
+        (acc) => acc.accountNumber == activeAccountNumber,
+      );
+      if (!senderAccount || !targetAccount) return UpdatedAccounts;
+      senderAccount.updateDebits(Number(amount), targetAccount);
+      return UpdatedAccounts;
+    });
   };
   const onCredit = (amount) => {
     if (!amount) return;
-    setAccounts((prev) =>
-      prev.map((acc) =>
-        acc.id == 1234
-          ? { ...acc, balance: acc.balance + Number(amount) }
-          : acc,
-      ),
-    );
+    setAccounts((prev) => {
+      const UpdatedAccounts = [...prev];
+      const account = UpdatedAccounts.find(
+        (acc) => acc.accountNumber == activeAccountNumber,
+      );
+      if (!account) return UpdatedAccounts;
+      account.updateCredits(Number(amount));
+      return UpdatedAccounts;
+    });
   };
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -2,52 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-function createBankAccount(userFullName, branchInfo, depositAmount) {
-  let branchName = branchInfo.branchName;
-  let branchCode = branchInfo.branchCode;
-  const dailyCreditLimit = 5000;
-  const dailyDebitLimit = 5000;
-  const monthlyCreditLimit = 150000;
-  const monthlyDebitLimit = 150000;
-  let userBalance = depositAmount;
-  let creditedToday = 0;
-  let debitedToday = 0;
-  let TransactionRecords = [];
-  function updateCredits(credits) {
-    if (credits + creditedToday <= dailyCreditLimit) {
-      userBalance += credits;
-      creditedToday += credits;
-      TransactionRecords.push({
-        userBalance,
-        actionType: " credit",
-        time: new Date(),
-      });
-    }
-  }
-  function updateDebits(debits, targetUseraccount) {
-    if (debits + debitedToday <= dailyDebitLimit) {
-      userBalance -= debits;
-      targetUseraccount += debits;
-      debitedToday += debits;
-      TransactionRecords.push({
-        userBalance,
-        actionType: "debit",
-        time: new Date(),
-      });
-    }
-  }
-  function TransactionHistory() {
-    return TransactionRecords;
-  }
-  return {
-    userFullName,
-    accountNumber: 675445266598,
-    IbanNumber: branchCode + "675445",
-    updateDebits,
-    updateCredits,
-    TransactionHistory,
-  };
-}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
